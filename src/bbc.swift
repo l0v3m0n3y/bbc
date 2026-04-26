089692ba25aecda1224c421dfd5471b0160d48e4
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-04-26 13:12:52 +0300

    Create bbc.swift

---
+++ src/bbc.swift
@@ -0,0 +1,60 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.unknown))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
+public class Bbc {
+    private let api = "https://web-cdn.api.bbci.co.uk"
+    private var headers: [String: String]
+    
+    public init() {
+        self.headers = [
+        "Accept":"*/*",
+        "Connection":"keep-alive",
+        "Accept-Encoding":"deflate, zstd",
+        "Accept-Language":"en-US,en;q=0.9",
+        "Host":"web-cdn.api.bbci.co.uk",
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+        ]
+
+    }
+    
+    public func get_news_list(path: String) async throws -> [String: Any]{
+        guard let url = URL(string: "\(api)/xd/page/content?path=\(path)") else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+    
+    public func get_news_by_assetId(type: String="live-header",assetId: String,language: String ="en-GB",showMedia: Bool=true) async throws -> [String: Any] {
+        guard let url = URL(string: "\(api)/wc-poll-data/container/\(type)?assetId=\(assetId)&globalContainerPolling=true&isInternational=true&isTipoPage=true&language=\(language)&liveExperienceCrowdCount=true&showMedia=\(showMedia)&uasEnv=live") else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+}
